47ed749691fc8169fae458394f347ba2744d78b1
Shreya <shreya186btcse23@igdtuw.ac.in>
2025-05-01 21:46:01 +0530

    update app.py

--- server/app.py
+++ server/app.py
@@ -139,6 +139,6 @@ def api_predict():
             "trace": traceback.format_exc()
         }), 400
     
-    if __name__ == '__main__':
-        port = int(os.environ.get('PORT', 5000))
-        app.run(host='0.0.0.0', port=port, debug=True)
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
